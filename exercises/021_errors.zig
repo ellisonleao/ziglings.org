1d0aac323bba59531009622512d5b45bbb835eb0
Ellison Leão <ellisonleao@gmail.com>
2025-01-26 20:54:29 -0300

    18 to 23

--- exercises/021_errors.zig
+++ exercises/021_errors.zig
@@ -1,15 +1,6 @@
-//
-// Believe it or not, sometimes things go wrong in programs.
-//
-// In Zig, an error is a value. Errors are named so we can identify
-// things that can go wrong. Errors are created in "error sets", which
-// are just a collection of named errors.
-//
-// We have the start of an error set, but we're missing the condition
-// "TooSmall". Please add it where needed!
 const MyNumberError = error{
     TooBig,
-    ???,
+    TooSmall,
     TooFour,
 };
 
@@ -26,7 +17,7 @@ pub fn main() void {
         if (number_error == MyNumberError.TooBig) {
             std.debug.print(">4. ", .{});
         }
-        if (???) {
+        if (number_error == MyNumberError.TooSmall) {
             std.debug.print("<4. ", .{});
         }
         if (number_error == MyNumberError.TooFour) {
